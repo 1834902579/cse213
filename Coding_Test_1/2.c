b03c19d9c77d4192594916b1329c5f19494afed0
Md-Masum <66910507+1834902579@users.noreply.github.com>
2020-08-07 22:21:44 -0700

    Update 2.c

--- Coding_Test_1/2.c
+++ Coding_Test_1/2.c
@@ -1,21 +1,25 @@
 #include<stdio.h>
+
 int main()
 {
     int array[100];
     int n,i;
+
     printf("Please Enter the size of Array : ");
     scanf("%d",&n);
 
-    for(i=0;i<n;i++){
-        scanf("%d",&array[i]);
+    printf("Please Enter elements in array : ");
+    for(i=0;i<n;++i)
+    {
+        scanf("%d",array+i);
     }
 
-    for(i=0;i<n;i++){
-        printf("\n%d\n",array[i]);
+    for(i=0;i<n;++i)
+    {
+        printf("\n%d\n",*(array+i));
     }
-
-
 
 
     return 0;
 }
+
